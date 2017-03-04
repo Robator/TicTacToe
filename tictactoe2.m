92e1e9301ec9abc637f23201fd582209cc7ed707
Robator <egor.matsuev@gmail.com>
2017-03-04 21:49:10 +0300

    4x4 tictactoe minimax, dummy AI

--- tictactoe2.m
+++ tictactoe2.m
@@ -1,9 +1,10 @@
 function tictactoe2()
 global summa
 summa = 0;
-    board = [0,0,0,0,0,0,0,0,0];
+%     board = [0,0,0,0,0,0,0,0,0];
+	board = zeros(1, 64);
     player=1;
-    for turn=1:9
+    for turn=1:64
 		%if there is no winner or not a draw
         if win(board) == 0		
             if rem(turn+player, 2) == 0	%reminder
@@ -50,12 +51,25 @@ end
 
 function win = win(board) %win combinations
 	%determines if a player has won, returns 0 otherwise.
-    wins = [1 2 3; 4 5 6; 7 8 9; 1 4 7; 2 5 8; 3 6 9; 1 5 9; 3 5 7];
-    for i=1:8
+	
+     wins1 = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16;
+        1 5 9 13; 2 6 10 14; 3 7 11 15; 4 8 12 16;
+        1 6 11 16; 4 7 10 13];%on plane 1
+    wins2 = wins1+16;
+    wins3 = wins2+16;%on plane 3
+    wins4 = wins3+16;%40 on plane 4
+    wins5 = [1 17 33 49; 2 18 34 50; 3 19 35 51; 4 20 36 52; 1 18 35 52; 4 19 34 49];%vertical plane 1
+    wins6 = wins5+4;
+    wins7 = wins6+4;%vertical plane 3
+    wins8 = wins7+4;%24
+    wins9 = [1 22 43 64; 4 23 42 61; 13 26 39 52; 16 27 38 49];%diagonals
+	wins = cat(1, wins1, wins2, wins3, wins4, wins5, wins6, wins7, wins8, wins9);%two-dimensional big array
+    for i=1:68
         if board(wins(i,1)) ~= 0 && ...
            board(wins(i,1)) == board(wins(i,2)) && ... 
-           board(wins(i,1)) == board(wins(i,3))
-            win = board(wins(i,3));
+           board(wins(i,1)) == board(wins(i,3)) && ...
+		   board(wins(i,1)) == board(wins(i,4))
+            win = board(wins(i,1));
             return
         end
     end
@@ -63,12 +77,16 @@ function win = win(board) %win combinations
 end
 
 function draw( b)
-        fprintf(' %c | %c | %c\n',gridChar(b(1)),gridChar(b(2)),gridChar(b(3)));
-        disp('---+---+---\n');
-        fprintf(' %c | %c | %c\n',gridChar(b(4)),gridChar(b(5)),gridChar(b(6)));
-        disp('---+---+---\n');
-        fprintf(' %c | %c | %c\n',gridChar(b(7)),gridChar(b(8)),gridChar(b(9)));
+	for i=1:16:48
+        fprintf(' %c | %c | %c | %c\n',gridChar(b(i)),gridChar(b(i+1)),gridChar(b(i+2)),gridChar(b(i+3)));
+        disp('---+---+----+-----\n');
+        fprintf(' %c | %c | %c | %c\n',gridChar(b(i+4)),gridChar(b(i+5)),gridChar(b(i+6)),gridChar(b(i+7)));
+        disp('---+---+----+-----\n');
+        fprintf(' %c | %c | %c | %c\n',gridChar(b(i+8)),gridChar(b(i+9)),gridChar(b(i+10)),gridChar(b(i+11)));
+		disp('---+---+----+-----\n');
+        fprintf(' %c | %c | %c | %c\n',gridChar(b(i+12)),gridChar(b(i+13)),gridChar(b(i+14)),gridChar(b(i+15)));
         disp('\n')
+	end
 end
 
 function a = minimax(board, player) 
@@ -82,7 +100,7 @@ function a = minimax(board, player)
 
     move = -1;
     score = -2;%Losing moves are preferred to no move
-    for i=1:9%For all moves,
+    for i=1:5%For all moves,
         summa=summa+1;
         if(board(i) == 0)%if empty
             board(i) = player;%try the move
@@ -105,11 +123,10 @@ end
 function board = computerMove(board) 
     move = -1;
     score = -2;
-    for i=1:9
+    for i=1:64
         if(board(i) == 0) 
             board(i) = 1;
             tempScore = -minimax(board, -1);
-%             disp(tempScore);
             board(i) = 0;
             if(tempScore > score) 
                 score = tempScore;
@@ -122,9 +139,20 @@ function board = computerMove(board)
 end
 
 function board = playerMove(board) 
-    move = 0;
-    while move > 9 || move <= 0 || board(move) ~= 0
-        move = input('\nInput move ([1..9]): ')
+    move = -1;
+    while move >= 64 || move < 0 || board(move) ~= 0
+        moveVec = input('\nInput move ([1..4, 1..4, 1..4]): ');
+		move = moveVec(1)+(moveVec(2)-1)*4 +(moveVec(3)-1)*16;
+		disp(move);
+        if(board(move)==0)
+            disp(board(move));
+            board(move) = -1;
+            disp(board(move));
+            return;
+        else
+            disp('cell is full, try again')
+            move = -1;
+		end
     end
     board(move) = -1;
 end
