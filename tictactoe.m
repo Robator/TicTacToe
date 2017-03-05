7d1236ecdec903053e0d3486bd02026de161b9b9
Vladimir Marochko <Stamp1993@users.noreply.github.com>
2017-03-05 19:13:43 +0300

    Added interface

--- tictactoe.m
+++ tictactoe.m
@@ -1,228 +1,221 @@
-function [] = tictactoe()
-    board = zeros(64);
-    player=2;
+function tictactoe2_board()
+
+%     board = [0,0,0,0,0,0,0,0,0];
+	board = zeros(1, 64);
+    player=1;
     for turn=1:64
-        if win(board) == 0
-            if rem(turn+player, 2) == 0
-                qStep(board);
-            else 
-                playerMove(board);
+		%if there is no winner or not a draw
+        if win(board) == 0		
+            if rem(turn+player, 2) == 0	%reminder
+				if turn==1
+					%computer`s first move to 0 cell
+                    moveVec = playTTT(board, 1);
+                    move = moveVec(1)+(moveVec(2)-1)*4 +(moveVec(3)-1)*16;
+					board(move) = -1;		
+				else
+					board = computerMove(board);
+				end
+			else 
+				draw(board);
+				board = playerMove(board);
+			end
+		end
+	end
+    write_winner(board);
+end
+
+function move = playTTT(board, player)
+    if(sum(board)==0)%first move - random
+    move = round(1+(rand*63));
+    move = (mtoV(move));
+    disp(move);
+    return;
+    end
+    if(player==1)
+        plr = -1;
+    else
+        plr = 1;
+    end
+    
+     move = -1;
+    score = -2;
+    moves = [];
+    for i=1:64
+        if(board(i) == 0) 
+            board(i) = 1;
+            tempScore = -minimax(board, plr);
+%             disp(tempScore);
+            board(i) = 0;
+            if(tempScore > score) 
+                score = tempScore;
+                moves = [];
+                moves = [moves i];
+            else
+                moves = [moves i];
             end
         end
-    end
-    switch win(board) 
+	end
+	%make a move that has the maximum score
+    move = datasample(moves, 1);
+    move = (mtoV(move));
+    disp(move);
+end
+
+function write_winner(board)
+switch win(board) 
         case 0
+			draw(board);
             disp('A draw. How droll.\n');
+			
         case 1
+            draw(board);
             disp('You lose.\n');
         case -1
+			draw(board);
             disp('You win. Inconceivable!\n');
     end
 end
 
+function c = gridChar(i) 
+    switch i 
+        case -1 
+            c='X';
+        case 0
+            c=' ';
+        case 1
+            c='O';
+    end
+end
 
-function [win] = win(board) 
-    wins1 = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16;
+function win = win(board) %win combinations
+	%determines if a player has won, returns 0 otherwise.
+	
+     wins1 = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16;
         1 5 9 13; 2 6 10 14; 3 7 11 15; 4 8 12 16;
-        1 6 11 16; 4 7 10 13];
+        1 6 11 16; 4 7 10 13];%on plane 1
     wins2 = wins1+16;
-    wins3 = wins2+16;
-    wins4 = wins3+16;%40
-    wins5 = [1 17 33 49; 2 18 34 50; 3 19 35 51; 4 20 36 52; 1 18 35 52; 4 19 34 49];
+    wins3 = wins2+16;%on plane 3
+    wins4 = wins3+16;%40 on plane 4
+    wins5 = [1 17 33 49; 2 18 34 50; 3 19 35 51; 4 20 36 52; 1 18 35 52; 4 19 34 49];%vertical plane 1
     wins6 = wins5+4;
-    wins7 = wins6+4;
+    wins7 = wins6+4;%vertical plane 3
     wins8 = wins7+4;%24
-    wins9 = [1 22 43 64; 4 23 42 61; 13 26 39 52; 16 27 38 49];
-    wins = cat(1, wins1, wins2, wins3, wins4, wins5, wins6, wins7, wins8, wins9);
-    win =0;
-    
+    wins9 = [1 22 43 64; 4 23 42 61; 13 26 39 52; 16 27 38 49];%diagonals
+	wins = cat(1, wins1, wins2, wins3, wins4, wins5, wins6, wins7, wins8, wins9);%two-dimensional big array
     for i=1:68
         if board(wins(i,1)) ~= 0 && ...
            board(wins(i,1)) == board(wins(i,2)) && ... 
            board(wins(i,1)) == board(wins(i,3)) && ...
-           board(wins(i,1)) == board(wins(i,4))
+		   board(wins(i,1)) == board(wins(i,4))
             win = board(wins(i,1));
-            break;
+            return
         end
     end
+    win = 0;
 end
 
-function [a] = minimax(board, player) 
-     winner = win(board);
-    if(winner ~= 0) 
-        a = winner*player;
-        return;
-    end
+function draw( b)
+	for i=0:16:49
+		for j=0:4:15
+        fprintf(' %c | %c | %c | %c\n',gridChar(b(i+j+1)),gridChar(b(i+j+2)),gridChar(b(i+j+3)),gridChar(b(i+j+4)));
+		disp('---+---+----+-----\n');
+		end
+		
+		disp('\n')
+	end
+end
 
+function a = minimax(board, depth, player) 
+
+	%How is the position like for player (their turn) on board?
+    winner = win(board);
+    if(winner ~= 0) %someone has won
+        a = winner*player;
+		return
+	end
+	if depth == 0
+		a=0;
+		return
+	end
     move = -1;
-    score = -2;
-    for i=1:64
-        if(board(i) == 0)
-            board(i) = player;
-            thisScore = -minimax(board, player*(-1));
+    score = -2;%Losing moves are preferred to no move
+    for i=1:64%For all moves,
+       
+        if(board(i) == 0)%if empty
+            board(i) = player;%try the move
+			thisScore = -minimax(board, depth-1, player*(-1));
+			if score == 1 && player==-1 || score == -1 && player==1%cant play better
+				break
+			end
+% 			fprintf('sum:%d\n', summa);
+% 			fprintf('d:%d\n', depth);
             if(thisScore > score) 
                 score = thisScore;
                 move = i;
-            end
-            board(i) = 0;
+			end
+% 			fprintf('%d(%d)\n',thisScore,i);
+            board(i) = 0;%Reset board after try
+
         end
     end
-    if(move == -1) 
+    if move == -1 %all cells are full
         a = 0;
-        return;
-    end
+        return
+	end
     a = score;
 end
 
+function board = computerMove(board) 
+    move = -1;
+    score = -2;
+	depth = 3;
+    for i=1:64
+        if(board(i) == 0) 
+            board(i) = 1;
+            tempScore = -minimax(board, depth, -1);
+            board(i) = 0;
+            if(tempScore > score) 
+                score = tempScore;
+                move = i;
+			end
+			if score == 1 %cant play better
+				break
+			end
+% 			fprintf('%d(%d) ', score, i);
+			if rem(i,4)==0
+				fprintf('\n');
+			end
+		end
+		
+	end
+	%make a move that has the maximum score
+    board(move) = 1;
+end
 
 function board = playerMove(board) 
     move = -1;
     while move >= 64 || move < 0 || board(move) ~= 0
         moveVec = input('\nInput move ([1..4, 1..4, 1..4]): ');
-        move = moveVec(1)+(moveVec(2)-1)*4-1 +(moveVec(3)-1)*16-1; 
-    end
-    board(move) = -1;
-end
-
-function Q = getQ(board)
- fid = fopen( 'values.txt');
-    tline = fgetl(fid);
-    i =1;
-    states = cell(0);
-    values = cell(0);
-    while ischar(tline)
-        input = strsplit(tline, '#');
-         states{i} =input(1);
-         values{i} = input(2);
-         tline = fgetl(fid);
-         i=i+1;
-    end
-        value = [];
-    if(size(states)==0)
-        found = 0;
-    else
-        found = 1;
-    end
-    if(found == 1)
-        has = 0;
-        for iter = 1:size(states)
-            state = states(iter,:);
-% 			here may be an error
-               if(size(setdiff(board, cell2mat([state{:}])))~=[1 1])
-                    value = values(iter);
-                    has =1;
-                    break;
-               end
-        end
-        if(has == 0)
-            value = zeros(size(board));
-            state = board;
-        end
-    else
-        value = zeros(size(board));
-        state = board;
-    end
-    
-    for it=1:64
-        if not(state(it)==0)
-            value(it) = -1000;
-        end
-    end
-    
-    Q = value;
-    fclose(fid);
-end
-
-function board = qStep(board)
-LR = 0.1;
-DF = 0.9;
-% value is 64x64
-  value = getQ(board);
-    
-    move = softmax(value);
-    
-    board(move) = 1;
-    reward = 0;
-    if(win(board) == 1)
-        reward = 1;
-    end
-    if(win(board) == -1)
-        reward = -1;
-    end
-    value(move,:) = value(move)+LR*(reward + DF*max(getQ(board))) - value(move);
-%     here is an error. undefined var STATE
-    saveQ(board, value);
-end
-
-function s = siqmEl(value, it)
-    s = exp(value(it))/sumSigm(value);
-end
-
-function sm = sumSigm(value)
-sm = 0;
-   for i=1:size(value)
-       sm = sm + exp(value(i));
-   end
-end
-
-function mv = softmax(value)
-    res = rand;
-    for i=1:64
-        des = siqmEl(value, i);
-        if(res<des)
-            mv = i;
+		move = moveVec(1)+(moveVec(2)-1)*4 +(moveVec(3)-1)*16;
+		disp(move);
+        if(board(move)==0)
+%             disp(board(move));
+            board(move) = 1;
+%             disp(board(move));
             return;
         else
-            res = res-des;
-        end
+            disp('cell is full, try again')
+            move = -1;
+		end
     end
+    board(move) = 1;
 end
 
-function [] = saveQ(state, value)
-fid = fopen( 'values.txt');
-    tline = fgetl(fid);
-    i =1;
-     states = cell(0);
-    values = cell(0);
-    while ischar(tline)
-        input = strsplit(tline, '#');
-        states{i} =input(1);
-        values{i} = input(2);
-        tline = fgetl(fid);
-        i=i+1;
-    end
-    
-    
-     if(size(states)==0)
-        found = 0;
-	 else
-        found = 1;
-     end
-    it = -1;
-    if(found == 1)
-        has = 0;
-        for iter = 1:states.size()
-            stateIn = states(iter);
-               if(state == stateIn)
-                    has =1;
-                    it = iter;
-                    break;
-               end
-        end
-        if(has == 0)
-             fid.write(state);
-        fid.write('#');
-        fid.write(value);
-        fid.write('\n');
-        else
-        fid.clear;
-        states(it) = state;
-            for i=1:states.size()
-            fid.write(states(i));
-            fid.write('#');
-            fid.write(values(i));
-            fid.write('\n');
-            end
-        end
-    end
+function res = mtoV(mv) 
+    k = ceil((mv)/16);
+    mv = mv - 16*(k-1);
+    j = ceil(mv/4);
+    mv = mv - 4*(j-1);
+    i = mv;
+    res = [i j k];
 end
