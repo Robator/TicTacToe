5ecbcc698a1338c579d612b8469368ebd80ab71c
Robator <egor.matsuev@gmail.com>
2017-03-05 23:39:30 +0300

    Some changes

--- test.m
+++ test.m
@@ -2,11 +2,9 @@ board=zeros(4,4,4);
 for turn=1:64
 	if rem(turn, 2) == 0
 		moveVec=playTTTT(board,1);
-		move = moveVec(1)+(moveVec(2)-1)*4 +(moveVec(3)-1)*16;
-		board(move) = 1;
+		board(moveVec(1),moveVec(2),moveVec(3)) = 1;
 	else
 		moveVec=playTTTT(board,2);
-		move = moveVec(1)+(moveVec(2)-1)*4 +(moveVec(3)-1)*16;
-		board(move) = 2;
+		board(moveVec(1),moveVec(2),moveVec(3)) = 2;
 	end
 end
